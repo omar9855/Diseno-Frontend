b856d6033107a76ed4fa25729d8217029734159b
Omar Castro <sonicomar98@gmail.com>
2023-09-11 21:47:03 -0700

    actualizacion fuerte de la pagina, asi como añadir footer y responsive

--- js/main.js
+++ js/main.js
@@ -1,36 +1,66 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
+document.addEventListener('DOMContentLoaded', () => {
+	const scrollToBottomButtom = document.getElementById('btn-ir-arriba');
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
-      scrollToBottomButtom.style.display = 'block';
-    } else {
-      scrollToBottomButtom.style.display = 'none';
-    }
-  })
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > 100) {
+			scrollToBottomButtom.style.display = 'block';
+		} else {
+			scrollToBottomButtom.style.display = 'none';
+		}
+	});
 
-  scrollToBottomButtom.addEventListener('click', () => {
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    })
-  })
-})
+	scrollToBottomButtom.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			// behavior: 'smooth'
+		});
+	});
+});
 
+//
+// imagenes de la reseña
+//
 
 const reviewImage = document.querySelector('.game-image');
-
+const largeImageContainer = document.createElement('div');
+largeImageContainer.classList.add('large-image-container');
+document.body.appendChild(largeImageContainer);
 
 reviewImage.addEventListener('click', () => {
+	const largeImage = document.createElement('img');
+	largeImage.src = reviewImage.src;
+	largeImage.classList.add('large-image');
 
-  const largeImage = document.createElement('img');
-  largeImage.src = reviewImage.src;
-  largeImage.classList.add('large-image');
+	largeImageContainer.innerHTML = ''; // Limpia el contenido existente
+	largeImageContainer.appendChild(largeImage);
+	largeImageContainer.style.display = 'block';
+});
 
-  document.body.appendChild(largeImage);
+largeImageContainer.addEventListener('click', (event) => {
+	if (event.target === largeImageContainer) {
+		largeImageContainer.style.display = 'none';
+	}
+});
 
-  largeImage.addEventListener('click', () => {
+//Formulario de contacto
 
-    document.body.removeChild(largeImage);
-  });
+const contactoForm = document.getElementById('contacto-form');
+
+contactoForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const nombre = document.getElementById('nombre').value;
+	const email = document.getElementById('email').value;
+	const mensaje = document.getElementById('mensaje').value;
+
+	alert(
+		'Gracias por contactarnos - Nombre: ' +
+			nombre +
+			'- Email: ' +
+			email +
+			'- Mensaje: ' +
+			mensaje
+	);
+
+	contactoForm.reset();
 });
